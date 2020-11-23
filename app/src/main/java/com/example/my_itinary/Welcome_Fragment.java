cfbc3b9ffce1724e04d6c4be340a80e6d78ab4ae
fahleung <61280996+fahleung@users.noreply.github.com>
2020-11-23 16:32:58 +0100

    sign in implementation

--- app/src/main/java/com/example/my_itinary/Welcome_Fragment.java
+++ app/src/main/java/com/example/my_itinary/Welcome_Fragment.java
@@ -6,11 +6,18 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import io.realm.mongodb.App;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
 
 
 /**
@@ -18,11 +25,14 @@ import android.widget.TextView;
  */
 public class Welcome_Fragment extends Fragment {
     public static FragmentManager fragmentManager;
+    EditText editLogin, editPassword;
     private Button connectButton;
     private TextView signButton;
+    private App app;
 
-    public Welcome_Fragment() {
+    public Welcome_Fragment(App app) {
         // Required empty public constructor
+        this.app = app;
     }
 
 
@@ -31,13 +41,38 @@ public class Welcome_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.welcome_fragment, container, false);
+        editLogin = (EditText) v.findViewById(R.id.user_log);
+        editPassword = (EditText) v.findViewById(R.id.pass_log);
         connectButton = v.findViewById(R.id.log_btn);
         signButton = v.findViewById(R.id.sign_btn);
 
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openHomeActivity();
+                //openHomeActivity();
+                if(!editLogin.getText().toString().isEmpty() && !editPassword.getText().toString().isEmpty())
+                {
+                    Credentials emailPasswordCredentials = Credentials.emailPassword(editLogin.getText().toString(), editPassword.getText().toString());
+                    app.loginAsync(emailPasswordCredentials, new App.Callback<io.realm.mongodb.User>() {
+                        @Override
+                        public void onResult(App.Result<User> result) {
+                            if(result.isSuccess())
+                            {
+                                //Log.v("User", "Logged success");
+                                openHomeActivity();
+                            }
+                            else
+                            {
+                                //Log.v("User", "Failed to log");
+                                Toast.makeText(getActivity(),"Login or Password incorrect.", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                        Toast.makeText(getActivity(),"Enter Login and Password please.", Toast.LENGTH_LONG).show();
+                }
 
             }
         });
@@ -58,4 +93,5 @@ public class Welcome_Fragment extends Fragment {
         Intent intent = new Intent(getActivity(), Home_activity.class);
         startActivity(intent);
     }
+
 }
