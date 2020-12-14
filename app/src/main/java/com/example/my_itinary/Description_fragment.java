7584812017add7c50ccdd10631057648d8a9557a
Marcoulin <utzmann.francois@gmail.com>
2020-12-14 19:56:37 +0100

    search_fragment + insert modif

---
+++ app/src/main/java/com/example/my_itinary/Description_fragment.java
@@ -0,0 +1,20 @@
+package com.example.my_itinary;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Description_fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_description_fragment, container, false);
+        return v;
+    }
+}
